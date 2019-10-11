06e0de2063b8169a62ffa4dd6d3c6f4000a9520a
elmernocon <elmernocon@gmail.com>
2019-10-11 16:21:59 +0800

    Tweaked samples.

--- automat/__main__.py
+++ automat/__main__.py
@@ -2,28 +2,44 @@ from automat import Automata
 
 
 def main():
+    # 0
+    zero = Automata.create_struct('0')
+    print('0')
+    print(zero)
+    print()
+
     # a*
     a = Automata.create_struct('a')
     a_star = Automata.create_kleene_star(a)
+    print('a*')
     print(a_star)
+    print()
 
     # b+
     b = Automata.create_struct('b')
     b_plus = Automata.create_kleene_plus(b)
+    print('b+')
     print(b_plus)
+    print()
 
     # c?
     c = Automata.create_struct('c')
     c_optional = Automata.create_optional(c)
+    print('c?')
     print(c_optional)
+    print()
 
     # a|b
     ab_union = Automata.create_union(a, b)
+    print('a|b')
     print(ab_union)
+    print()
 
     # ab
     ab_concat = Automata.create_concat(a, b)
+    print('ab')
     print(ab_concat)
+    print()
 
 
 if __name__ == '__main__':
